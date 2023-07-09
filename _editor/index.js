964c75e7dfb349fc9a53058bdc2e1bb6e919dde0
Mohit Karekar <karekar.mohit@gmail.com>
2023-07-09 22:05:55 +0200

    Update editor

--- _editor/index.js
+++ _editor/index.js
@@ -5,8 +5,27 @@ import List from '@editorjs/list';
 import edjsParser from 'editorjs-parser';
 import { NodeHtmlMarkdown } from 'node-html-markdown';
 
-fetch('http://localhost:5000/content/tree').then((data) => {
+function constructTree(el, dirChildren, marginLeft = 0) {
+    const ul = document.createElement("ul");
+    dirChildren.forEach((dir) => {
+        const li = document.createElement("li");
+        li.innerHTML = dir.name;
+        li.style.marginLeft = marginLeft + 'px';
+        if (dir.children)
+            constructTree(li, dir.children, marginLeft + 2);
+        ul.appendChild(li);
+    })
+    el.appendChild(ul);
+}
+
+fetch('http://localhost:5000/content/tree').then(d => d.json()).then((data) => {
     console.log(data);
+    const dirTree = document.querySelector('#dir-tree');
+    if (!dirTree) return;
+    const ul = document.createElement("ul");
+    ul.innerHTML = data.name;
+    constructTree(ul, data.children);
+    dirTree.appendChild(ul);
 })
 
 const editor = new EditorJS({
@@ -34,6 +53,10 @@ const parser = new edjsParser();
 document.querySelector('#save-button').addEventListener('click', () => {
     editor.save().then(data => {
         const html = parser.parse(data);
+        fetch({
+            method: 'POST',
+            url: "/content/"
+        })
         console.log(NodeHtmlMarkdown.translate(html))
     })
 })
